fb781fb0d252168d5a6fb5c3224d2867c0e4658a
Mike Brady <git@mikebrady.com>
2023-12-29 09:18:03 -0500

    v1.1.0 Release

--- src/control.lua
+++ src/control.lua
@@ -1,14 +1,22 @@
-function cursor_stack_to_ghost(player)
-	if player.cursor_stack and player.cursor_stack.valid and player.cursor_stack.valid_for_read and player.cursor_stack.prototype.place_result then
-		player.get_main_inventory().insert({name=player.cursor_stack.name, count=player.cursor_stack.count})
-		player.cursor_ghost = player.cursor_stack.name
-		player.cursor_stack.clear()
+function stack_to_ghost(player)
+	if player.cursor_stack and player.cursor_stack.valid and player.cursor_stack.valid_for_read then
+		if player.cursor_stack.prototype.place_result or player.cursor_stack.prototype.place_as_tile_result then
+			player.get_main_inventory().insert(player.cursor_stack)
+			player.cursor_ghost = player.cursor_stack
+			player.cursor_stack.clear()
+		end
 	end
 end
 
-function cursor_ghost_to_stack(player)
-	if player.cursor_ghost then
-		player.pipette_entity(player.cursor_ghost.place_result)
+function ghost_to_stack(player)
+	if player.cursor_ghost and player.cursor_ghost.valid then
+		local stack, idx = player.get_main_inventory().find_item_stack(player.cursor_ghost.name)
+		if stack then
+			player.cursor_stack.transfer_stack(stack)
+		else
+			player.cursor_stack.clear()
+		end
+		player.cursor_ghost = nil
 	end
 end
 
@@ -19,9 +27,9 @@ function toggle_ghost_mode(player)
 	player.set_shortcut_toggled('ghost_mode_toggle_shortcut', not state)
 
 	if state then
-		cursor_ghost_to_stack(player)
+		ghost_to_stack(player)
 	else
-		cursor_stack_to_ghost(player)
+		stack_to_ghost(player)
 	end
 end
 
@@ -32,6 +40,7 @@ script.on_event('ghost_mode_toggle_custom_input',
 		toggle_ghost_mode(player)
 	end
 )
+
 
 
 script.on_event(defines.events.on_lua_shortcut,
@@ -47,7 +56,7 @@ script.on_event(defines.events.on_player_cursor_stack_changed,
 	function(event)
 		local player = game.get_player(event.player_index)
 		if player.is_shortcut_toggled('ghost_mode_toggle_shortcut') then
-			cursor_stack_to_ghost(player)
+			stack_to_ghost(player)
 		end
 	end
 )
