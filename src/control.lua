84704c7764b8d731871506afaba5adebbe6756ae
Mike Brady <git@mikebrady.com>
2024-11-10 08:52:43 -0500

    Added on_player_pipette event handler to pipette ghost items when ghost mode is on.

--- src/control.lua
+++ src/control.lua
@@ -1,9 +1,13 @@
+function set_cursor_ghost(player, item)
+	player.clear_cursor()
+	player.cursor_ghost = item
+end
+
+
 function stack_to_ghost(player)
 	if player.cursor_stack and player.cursor_stack.valid and player.cursor_stack.valid_for_read then
 		if player.cursor_stack.prototype.place_result or player.cursor_stack.prototype.place_as_tile_result then
-			local stack = player.cursor_stack.name
-			player.clear_cursor()
-			player.cursor_ghost = stack
+			set_cursor_ghost(player, player.cursor_stack.name)
 		end
 	end
 end
@@ -54,6 +58,15 @@ script.on_event(defines.events.on_player_cursor_stack_changed,
 		local player = game.get_player(event.player_index)
 		if player.is_shortcut_toggled('ghost_mode_toggle_shortcut') then
 			stack_to_ghost(player)
+		end
+	end
+)
+
+script.on_event(defines.events.on_player_pipette,
+	function(event)
+		local player = game.get_player(event.player_index)
+		if player.is_shortcut_toggled('ghost_mode_toggle_shortcut') then
+			set_cursor_ghost(player, event.item.name)
 		end
 	end
 )
